33bcb9cbffa9527ede7d9957cd484e2ddfd2e116
Neil-urk12 <neilvallecer12@gmail.com>
2025-04-11 16:15:44 +0800

    feat: Implemented the simplest approach in finding the k-th smallest/largest in a sample/list

--- simpleApproach.py
+++ simpleApproach.py
@@ -1,6 +1,12 @@
-class SimplestApproach:
-    def findKthSmallestOrLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k
-
-        def quickSelect(left, right):
-
+"""
+    The function finds the k-th smalles or largest element in a list/sample in the simplest way possible.
+    Which is to sort the list and return the k-th element.
+    This is not the most efficient way, but it is the simplest.
+    Parameters:
+        nums (list[int]): The list of integers to find the k-th element from.
+        k (int): The index of the element to find.
+    Returns:
+        int: The k-th smallest or largest element in the list.
+"""
+def findKthSmallestOrLargest(self, nums: list[int], k: int) -> int:
+    return sorted(nums)[k]
